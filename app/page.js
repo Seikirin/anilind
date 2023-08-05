9389a24a53151e106c28a7f85564497032d804dd
Seikirin <81483357+Seikirin@users.noreply.github.com>
2023-08-05 21:19:15 +0100

    a

--- app/page.js
+++ app/page.js
@@ -116,6 +116,11 @@ function getAiringDay(anime) {
   return airingDay ? new Date(airingDay * 1000).toLocaleDateString('en-US', { weekday: 'long' }) : ''
 }
 
+function getAiringHourInLocalTimezone(anime) {
+  const airingDay = anime.media.nextAiringEpisode?.airingAt
+  return airingDay ? new Date(airingDay * 1000).toLocaleTimeString('en-US', { hour: 'numeric', hour12: true }) : ''
+}
+
 function requestAnilistIncreaseProgressByOne(anime, session, setChanged, setList) {
   const { accessToken } = session;
 
@@ -284,7 +289,6 @@ function AnimeCard({ mediaId, session, setChanged, setList, list }) {
           Watched
         </div>
       </div>}
-
       <div
         ref={innerRef}
         style={{ opacity: opacity }}
@@ -296,10 +300,20 @@ function AnimeCard({ mediaId, session, setChanged, setList, list }) {
         </div>
         <div className="absolute inset-0 p-0 md:p-2 flex items-end  -translate-y-0.5">
           <div className="w-2/5 md:1/3 h-full flex items-center justify-center py-0.5">
-            <img src={anime.media.coverImage.large} alt={anime.media.title.romaji} className="max-w-full md:aspect-[27/38] h-full w-full md:h-auto md:rounded object-cover" />
+            <div className="max-w-full md:aspect-[27/38] h-full w-full md:h-auto md:rounded overflow-hidden relative">
+              <img src={anime.media.coverImage.large} alt={anime.media.title.romaji} className="w-full h-full object-cover" />
+              {getAiringDay(anime) && <div className="absolute inset-0 top-3/4 bg-black bg-opacity-60 p-1 flex items-center justify-center flex-col">
+                  <div className="text-white text-center text-xs w-full leading-3">
+                    {getAiringDay(anime)}
+                  </div>
+                  <div className="text-white text-center text-opacity-75 text-[0.6rem] w-full leading-[0.6rem]">
+                    {getAiringHourInLocalTimezone(anime)}
+                  </div>
+              </div>}
+            </div>
           </div>
           <div className="w-3/5 md:2/3 h-1/2 p-2 flex flex-col">
-            <div className="truncate text-opacity-90 text-white mt-0.5">
+            <div className="truncate text-opacity-90 text-white mt-0.5 flex-shrink-0">
               {anime.media.title.english || anime.media.title.romaji}
             </div>
             <div className="text-xs text-opacity-50 text-white">
@@ -369,7 +383,11 @@ function LoadingCard() {
 }
 
 function AnimeList({ list, filterFunction, title, className, session, setChanged, dataState, setList, loadingComponents }) {
-  const newList = list.filter(filterFunction).sort((a, b) => a.media.nextAiringEpisode.timeUntilAiring - b.media.nextAiringEpisode.timeUntilAiring)
+  const newList = list.filter(filterFunction).sort((a, b) => {
+  const aTime = a.media.nextAiringEpisode?.timeUntilAiring;
+  const bTime = b.media.nextAiringEpisode?.timeUntilAiring;
+  return (aTime || Infinity) - (bTime || Infinity) || getEpisodesBehind(a) - getEpisodesBehind(b);
+});
 
   return (
     <div className={`flex flex-wrap gap-4 p-8 ${className}`}>
@@ -404,7 +422,9 @@ function AnimeList({ list, filterFunction, title, className, session, setChanged
 export default function Home() {
   const { changed, setChanged, setDataState, dataState } = useContext(DataContext)
   const { data: session, status } = useSession()
-  const [username, setUsername] = useState(session?.user.name || 'seikirin')
+  const [username, setUsername] = useState(
+      session?.user?.name    
+   || 'seikirin')
   const [list, setList] = useState([])
   const loadingComponents = useRef(<LoadingCard />)
   const weekDaysStartingWithToday = getWeekDaysStartingWithToday();
@@ -413,7 +433,7 @@ export default function Home() {
     if (username.length > 0) {
       setList([])
       getAnilistUserWatchingList(username, setDataState, session).then((newList) => {
-        const toReturn = newList ? setList([...newList].filter(anime => anime.media.nextAiringEpisode)) : setList([])
+        const toReturn = newList ? setList([...newList]) : setList([])
         const listOfOnlyBehind = newList.filter(anime => getEpisodesBehind(anime) > 0)
         loadingComponents.current = Array.from({ length: Math.max(listOfOnlyBehind.length, 1) }, (_, i) => <LoadingCard key={i} />)
         return toReturn;
